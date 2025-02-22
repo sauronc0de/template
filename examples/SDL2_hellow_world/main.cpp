395b1977b7796056b62abaf40e826fbbcc0fd17b
sauron <sauroncode@gmail.com>
2025-02-22 19:29:20 +0000

    Trying to show a png. Added clang format and some docker updates

--- examples/SDL2_hellow_world/main.cpp
+++ examples/SDL2_hellow_world/main.cpp
@@ -3,31 +3,54 @@
 // compile with: clang++ main.cpp -o hello_sdl2 -lSDL2
 // run with: ./hello_sdl2
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <ostream>
+#include <string>
+
+SDL_Texture *playerTex;
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-int main(int argc, char* args[]) {
-  SDL_Window* window = NULL;
-  SDL_Surface* screenSurface = NULL;
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+int main(int argc, char *args[])
+{
+  std::string assetPath = ASSETS_PATH; // Provided by CMake
+  std::string imagePath = assetPath + "/img/surrender.png";
+
+  SDL_Window *window = NULL;
+  SDL_Surface *screenSurface = NULL;
+
+  if(SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
     fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
     return 1;
   }
-  window = SDL_CreateWindow(
-			    "hello_sdl2",
-			    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-			    SCREEN_WIDTH, SCREEN_HEIGHT,
-			    SDL_WINDOW_SHOWN
-			    );
-  if (window == NULL) {
+  window = SDL_CreateWindow("hello_sdl2", SDL_WINDOWPOS_UNDEFINED,
+                            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                            SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if(window == NULL)
+  {
     fprintf(stderr, "could not create window: %s\n", SDL_GetError());
     return 1;
   }
   screenSurface = SDL_GetWindowSurface(window);
-  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+  SDL_FillRect(screenSurface, NULL,
+               SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
   SDL_UpdateWindowSurface(window);
-  SDL_Delay(2000);
+
+  // Add render here
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_Surface *tmpSurface = IMG_Load(imagePath.c_str());
+  std::cout << imagePath << std::endl;
+  std::cout << "Assets are located at: " << ASSETS_PATH << std::endl;
+  playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+  SDL_FreeSurface(tmpSurface);
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, playerTex, NULL, NULL);
+  SDL_RenderPresent(renderer);
+
+  SDL_Delay(20000);
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
