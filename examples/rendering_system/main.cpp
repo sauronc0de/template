e670db4e8800ce16763a2dc4d89091c2e26ab8ef
sauron <sauroncode@gmail.com>
2025-02-22 23:38:00 +0000

    Created rendering system library and example. The example works properly but Clang show some errors. Errors doesn't match with the real build, pending to check

---
+++ examples/rendering_system/main.cpp
@@ -0,0 +1,12 @@
+#include "rendering_system.h"
+
+int main(int argc, char *args[])
+{
+  {
+    //* Create rendering system
+    RenderingSystemAO *renderingSystem = new RenderingSystemAO();
+  }
+  //* Destroy rendering system
+
+  return 0;
+}
